8c786634784497436bfe1817e16655b013b7cb27
Jed <jed324@gmail.com>
2023-03-03 18:27:54 -0800

    Go forth

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void TutorialBtn()
+    {
+        SceneManager.LoadScene("JedScene");
+    }
+
+    public void FarmBtn()
+    {
+        SceneManager.LoadScene("JedScene");
+    }
+
+    public void SpaceshipBtn()
+    {
+        SceneManager.LoadScene("JedScene");
+    }
+    
+}
